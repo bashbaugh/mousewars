4b9d876b68cbe6dada624888bbfe8f474e6a6691
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-08-10 15:54:40 -0600

    aaa hit testing no work

--- app/src/Game/Game.tsx
+++ app/src/Game/Game.tsx
@@ -9,48 +9,72 @@ const SEGMENT_LENGTH = 5
 
 const client = new Colyseus.Client('ws://localhost:3001')
 
-function draw() {
+class Snake {
+  path: paper.Path
+
+  constructor (color: string) {
+    this.path = new Paper.Path({
+      strokeColor: color,
+      strokeWidth: 20,
+      strokeCap: 'round'
+    })
+
+    const start = Paper.view.center.divide(new Paper.Point(10, 1))
+    for (let i = 0; i < STARTING_SEGMENTS; i++)
+      this.path.add(start.add(new Paper.Point(i * SEGMENT_LENGTH, 0)))
+  }
+
+  update (playerState: any) {
+    this.path.strokeColor = playerState.color
+    this.path.firstSegment.point = new Paper.Point(playerState.mouseX, playerState.mouseY)
+    for (var i = 0; i < STARTING_SEGMENTS - 1; i++) {
+      var segment = this.path.segments[i]
+      var nextSegment = segment.next
+      var vector = segment.point.subtract(nextSegment.point)
+      vector.length = SEGMENT_LENGTH
+      nextSegment.point = segment.point.subtract(vector)
+    }
+    this.path.smooth({ type: 'continuous' })
+
+    console.log(
+      this.path.hitTestAll(this.path.firstSegment.point)
+      // .map(hr => hr.item.data)
+      .forEach(hr => {
+        
+      })
+      // .filter(hitResult => hitResult.item.data.playerId !== client.id)
+    )
+  }
+}
+
+function initialize() {
   // tool.onMouseMove = (e: paper.ToolEvent) => {
   //   blobs.push(new Blob(e))
   // }
 
   let room: Colyseus.Room
+  let snakes: Record<string, Snake> = {}
 
   client.joinOrCreate("snake").then(r => {
     console.log(r.sessionId, "joined", r.name);
     room = r
+
+    room.onStateChange(state => {
+      for (const player of state.players.values()) {
+        if (!snakes[player.clientId]) snakes[player.clientId] = new Snake(player.color)
+        snakes[player.clientId].update(player)
+      }
+    })
   }).catch(e => {
       console.log("JOIN ERROR", e);
   })
 
-  const path = new Paper.Path({
-    strokeColor: '#E4141B',
-    strokeWidth: 20,
-    strokeCap: 'round'
-  })
-
-  var start = Paper.view.center.divide(new Paper.Point(10, 1))
-  for (var i = 0; i < STARTING_SEGMENTS; i++)
-	  path.add(start.add(new Paper.Point(i * SEGMENT_LENGTH, 0)))
-
   Paper.view.on('mousemove', (e: paper.ToolEvent) => {
-    path.firstSegment.point = e.point;
-    for (var i = 0; i < STARTING_SEGMENTS - 1; i++) {
-      var segment = path.segments[i];
-      var nextSegment = segment.next;
-      var vector = segment.point.subtract(nextSegment.point)
-      vector.length = SEGMENT_LENGTH;
-      nextSegment.point = segment.point.subtract(vector)
-    }
-    path.smooth({ type: 'continuous' });
-    room?.send('path', { 
-      segments: path.segments
-    })
-    
+    room?.send('mousePoint', { point: e.point })
   })
 
   Paper.view.on('frame', (e: any) => {
-
+    
   })
 }
 
@@ -60,7 +84,7 @@ const MouseBlobs: React.FC = props => {
   useEffect(() => {
     Paper.setup(ref.current!)
 
-    draw()
+    initialize()
 
     return () => {
       Paper.view.remove()
--- server/SnakeRoom.ts
+++ server/SnakeRoom.ts
@@ -1,24 +1,88 @@
 import { Room, Client } from "colyseus"
-import { Schema, type, ArraySchema } from "@colyseus/schema"
+import { Schema, type, MapSchema } from "@colyseus/schema"
+import Paper from 'paper'
+import { Size } from "paper/dist/paper-core"
+
+Paper.setup(new Paper.Size([2000, 2000]))
+
+const STARTING_SEGMENTS = 100
+const SEGMENT_LENGTH = 5
 
 class Player extends Schema {
-  @type('string')
-  clientId: string // We'll use this to keep track of which player is which
+  constructor (clientId: string) {
+    super()
+    this.clientId = clientId
+    this.color = Paper.Color.random().toCSS(true)
+  }
+
+  @type('string') clientId: string
+  @type('string') color: string
+
+  @type('uint16') mouseX: number = 50
+  @type('uint16') mouseY: number = 50
 }
 
 class PongState extends Schema {
-  @type([Player]) players = new ArraySchema<Player>()
+  @type({ map: Player }) players = new MapSchema<Player>()
 }
 
-export default class SnakeRoom extends Room {
+class Snake {
+  path: paper.Path
+
+  constructor (player: Player) {
+    this.path = new Paper.Path({
+      strokeColor: player.color,
+      strokeWidth: 20,
+      strokeCap: 'round',
+      data: {
+        playerId: player.clientId
+      }
+    })
+
+    const start = Paper.view.center.divide(new Paper.Point(10, 1))
+    for (let i = 0; i < STARTING_SEGMENTS; i++)
+      this.path.add(start.add(new Paper.Point(i * SEGMENT_LENGTH, 0)))
+  }
+
+  update (playerState: Player) {
+    this.path.firstSegment.point = new Paper.Point(playerState.mouseX, playerState.mouseY)
+    for (var i = 0; i < STARTING_SEGMENTS - 1; i++) {
+      var segment = this.path.segments[i]
+      var nextSegment = segment.next
+      var vector = segment.point.subtract(nextSegment.point)
+      vector.length = SEGMENT_LENGTH
+      nextSegment.point = segment.point.subtract(vector)
+    }
+    this.path.smooth({ type: 'continuous' })
+  }
+}
+
+export default class SnakeRoom extends Room<PongState> {
+  snakes: Record<string, Snake> = {}
+
   onCreate (options: any) {
     this.setState(new PongState()) // Set the state for the room
-    this.setSimulationInterval(delta => this.update(delta)) // Set a "simulation interval" aka an update function (similar to the loop function in game.js)
-    this.setPatchRate(40) // The patch rate determines the interval (in milliseconds) at which the server sends state updates to the client
-    this.maxClients = 2 // Only 2 players per Pong game
+    this.setSimulationInterval(delta => this.update(delta), 30) // Set a "simulation interval" aka an update function (similar to the loop function in game.js)
+    this.setPatchRate(30)
+    this.maxClients = 5 // Only 2 players per Pong game
 
-    this.onMessage('path', (client, msg) => {
-      console.log(msg)
+    this.onMessage('mousePoint', (client, msg) => {
+      const p = this.state.players.get(client.id)
+      p.mouseX = msg.point[1]
+      p.mouseY = msg.point[2]
+
+      const s = this.snakes[client.id]
+      s.update(p)
+
+      let a: any
+
+      // a?.remove()
+
+      console.log(
+        s.path.hitTestAll(s.path.firstSegment.point)
+        .map(hr => hr.item.data)
+        // .filter(hitResult => hitResult.item.data.playerId !== client.id)
+      )
     })
   }
 
@@ -31,7 +95,9 @@ export default class SnakeRoom extends Room {
   }
 
   onJoin (client: Client, options: any) {
-
+    const p = new Player(client.id)
+    this.state.players.set(client.id, p)
+    this.snakes[client.id] = new Snake(p)
   }
 
   onLeave (client: Client, consented: boolean) {
